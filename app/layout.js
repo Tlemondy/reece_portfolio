7c4908ddc2c798626930f8b7fb0202d90a0f470c
Reece Cussons <reececussons@gmail.com>
2026-02-09 12:10:30 +0200

    Add favicon

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,9 @@ import './globals.css'
 export const metadata = {
   title: 'Reece - Portfolio',
   description: 'Personal portfolio and freelancing services',
+  icons: {
+    icon: 'https://avatars.githubusercontent.com/u/96705270?v=4',
+  },
 }
 
 export default function RootLayout({ children }) {
